e74af78493e02d06334ca9319bb6ccb4bb6bc982
Shreshth Gandhi <shreshth@psi.toronto.edu>
2016-11-02 18:53:17 -0400

    completed support for variable length sequences and got perfect results

--- DNA_reader.py
+++ DNA_reader.py
@@ -4,28 +4,28 @@ import numpy as np
 # Token convention: GO 5, EOS 6, PAD 7
 
 
-def DNA_producer(raw_data, batch_size, num_steps_encoder, num_steps_decoder, name=None):
-    with tf.name_scope(name, "DNA_Producer", [raw_data, batch_size, num_steps_encoder, num_steps_decoder]):
-        raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
-        data_size = tf.shape(raw_data)[0]  # Total number of sequences
-        num_batches = data_size // batch_size
-        data = raw_data[0:batch_size * num_batches]
+# def DNA_producer(raw_data, batch_size, num_steps_encoder, num_steps_decoder, name=None):
+#     with tf.name_scope(name, "DNA_Producer", [raw_data, batch_size, num_steps_encoder, num_steps_decoder]):
+#         raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
+#         data_size = tf.shape(raw_data)[0]  # Total number of sequences
+#         num_batches = data_size // batch_size
+#         data = raw_data[0:batch_size * num_batches]
 
-        GO_tensor = tf.constant(5, dtype=tf.int32, shape=[batch_size, 1])
-        EOS_tensor = tf.constant(6, dtype=tf.int32, shape=[batch_size, 1])
-        PAD_tensor = tf.constant(7, dtype=tf.int32, shape=[batch_size, num_steps_decoder-num_steps_encoder - 2])
-        PAD_tensor_target = tf.constant(7, dtype=tf.int32, shape=[batch_size,num_steps_decoder-num_steps_encoder-1])
-        # data = tf.reshape(raw_data[0: batch_size * num_batches], [batch_size, num_batches])
-        i = tf.train.range_input_producer(num_batches, shuffle= False).dequeue()
-        encoder_input = tf.slice(data,[i * batch_size, 0 ],[batch_size,num_steps_encoder])
-        decoder_input = tf.concat(1, [GO_tensor,
-                                  tf.slice(data,[i * batch_size, 0 ],[batch_size,num_steps_encoder]),
-                                  EOS_tensor,PAD_tensor])
-        decoder_targets = tf.concat(1, [
-                                  tf.slice(data,[i * batch_size, 0 ],[batch_size,num_steps_encoder]),
-                                  EOS_tensor,PAD_tensor_target])
+#         GO_tensor = tf.constant(5, dtype=tf.int32, shape=[batch_size, 1])
+#         EOS_tensor = tf.constant(6, dtype=tf.int32, shape=[batch_size, 1])
+#         PAD_tensor = tf.constant(7, dtype=tf.int32, shape=[batch_size, num_steps_decoder-num_steps_encoder - 2])
+#         PAD_tensor_target = tf.constant(7, dtype=tf.int32, shape=[batch_size,num_steps_decoder-num_steps_encoder-1])
+#         # data = tf.reshape(raw_data[0: batch_size * num_batches], [batch_size, num_batches])
+#         i = tf.train.range_input_producer(num_batches, shuffle= False).dequeue()
+#         encoder_input = tf.slice(data,[i * batch_size, 0 ],[batch_size,num_steps_encoder])
+#         decoder_input = tf.concat(1, [GO_tensor,
+#                                   tf.slice(data,[i * batch_size, 0 ],[batch_size,num_steps_encoder]),
+#                                   EOS_tensor,PAD_tensor])
+#         decoder_targets = tf.concat(1, [
+#                                   tf.slice(data,[i * batch_size, 0 ],[batch_size,num_steps_encoder]),
+#                                   EOS_tensor,PAD_tensor_target])
 
-    return encoder_input, decoder_input, decoder_targets
+#     return encoder_input, decoder_input, decoder_targets
 
 
 # def DNA_read(data_size=10000, seq_length=10, vocab_size=4):
@@ -46,7 +46,40 @@ def DNA_read(data_size=10000, seq_length=10, vocab_size=4):
     for step in range(data_size):
         idx = step * vocab_size // (data_size)
         length = np.random.random_integers(seq_length // 2, seq_length)
-        entry = np.full([length],idx, dtype=np.int32)
+        entry = np.full([length],idx, dtype=np.int64)
         x.append(entry)
     np.random.shuffle(x)
     return x
+
+def DNA_producer(raw_data, config, name=None):
+    batch_size = config.batch_size
+    num_steps_encoder = config.num_steps_encoder
+    num_steps_decoder = config.num_steps_decoder
+    vocab_size = config.vocab_size
+
+    with tf.name_scope(name, "DNA_Producer"):
+        data_size = len(raw_data)
+        num_batches = data_size // batch_size
+        data = raw_data[0: batch_size*num_batches]
+        # vocab_size -1 is 7 in our case and means PAD symbol
+        encoder_input_full = np.full([data_size, num_steps_encoder], vocab_size-1)
+        decoder_input_full = np.full([data_size, num_steps_decoder], vocab_size-1)
+        decoder_targets_full = np.full([data_size, num_steps_decoder], vocab_size-1)
+        for (i, seq) in enumerate(data):
+            length = seq.shape[0]
+            encoder_input_full[i, 0:length] = seq
+            
+            decoder_targets_full[i, 0:length] = seq
+            decoder_targets_full[i, length] = vocab_size - 2  # For EOS
+            
+            decoder_input_full[i, 0] = vocab_size - 3
+            decoder_input_full[i, 1:length + 1] = seq
+            decoder_input_full[i, length + 1] = vocab_size - 2
+        encoder_input_full = tf.convert_to_tensor(encoder_input_full, name="encoder_input", dtype=tf.int32)
+        decoder_input_full = tf.convert_to_tensor(decoder_input_full, name="decoder_input", dtype=tf.int32)
+        decoder_targets_full = tf.convert_to_tensor(decoder_targets_full, name="decoder_targets", dtype=tf.int32)
+        i = tf.train.range_input_producer(num_batches, shuffle=False).dequeue()
+        encoder_input = tf.slice(encoder_input_full, [i * batch_size, 0 ],[batch_size,num_steps_encoder])
+        decoder_input = tf.slice(decoder_input_full, [i * batch_size, 0 ],[batch_size,num_steps_decoder])
+        decoder_targets = tf.slice(decoder_targets_full, [i * batch_size, 0 ],[batch_size,num_steps_decoder])
+    return encoder_input, decoder_input, decoder_targets
